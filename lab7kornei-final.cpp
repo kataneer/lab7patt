482acb406766b2e0208ea98f86ac03571300d268
Anton <87985384+kataneer@users.noreply.github.com>
2021-12-20 14:33:10 +0700

    Update lab7kornei-final.cpp

--- lab7kornei-final.cpp
+++ lab7kornei-final.cpp
@@ -20,51 +20,35 @@
 
 using namespace std;
 
-class extHydrate {
-};
-
-
-class extWeather : public extHydrate {
-public:
-	weatherCounter *weather = new weatherCounter();
-};
-
-class extSnow : public extHydrate {
-public:
-	snowCounter *snow = new snowCounter();
-};
-
-class extIce : public extHydrate {
-public:
-	iceCounter *ice = new iceCounter();
-};
-
-
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "Russian");
-	metCounter *conto = new metCounter();
-	Watch *seeyo = new Watch(36, 116);
-	flyweightFactory *eski = new flyweightFactory();
 	printf("Bridge\n\n");
+//обращаемся через экземпляр класса Watch к методу graphFlow у класса Watcher и graphForecast класса ProxyWatcher для проверки связей
+	Watch *seeyo = new Watch(36, 116);
 	printf("С обычного наблюдателя, flow:\n\n");
-	seeyo->watchi->graphFlow();
+	seeyo->watchi->graphFlow();                 
 	printf("С как бы прокси-наблюдателя, forecast:\n\n");
 	seeyo->proxywatchi->graphForecast();
-	conto->init(35, 123.4);
-	conto->fieldWay->init(30.4);
 	printf("Flyweight\n\n");
+//создаем фабрику eski, через встроенный метод создания - множество объектов-счетчиков с разными параметрами и выводим информацию по ним
+       flyweightFactory *eski = new flyweightFactory();
 	eski->creati("Береговая", "snow", 1, 1, 123.4, 35);
 	eski->creati("Береговая", "snow", 2, 1, 125, 37);
 	eski->creati("Ленточный бор", "weather", 1, 1, 125, 37);
 	printf("Facade\n\n");
+//создаем новый экземпляр класса Facade и обращаемся через унифицированный интерфейс к методу waterLvl из связанного класса
 	Facade *k = new Facade();
 	k->FacadeWater->waterLvl();
 	printf("Information expert\n\n");
+//подсчитываем циклоны, поля давлений и температуры в отдельном классе metCyclons и его методах подсчета cyclonsCount, atFieldCount и tempFieldCount через экземпляр класса fieldWay
+	metCounter *conto = new metCounter();
+	conto->init(35, 123.4);
+	conto->fieldWay->init(30.4);
 	conto->fieldWay->cyclonsCount(conto->atm, conto->temp);
 	conto->fieldWay->atFieldCount(conto->atm, conto->temp);
 	conto->fieldWay->tempFieldCount(conto->atm, conto->temp);
 	_getch();
 	return 0;
 }
+
