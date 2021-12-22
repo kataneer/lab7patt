3aafd0a21891a30403a2e956bff6ecb90cf92fcc
Anton <87985384+kataneer@users.noreply.github.com>
2021-12-22 11:24:27 +0700

    Update facade.cpp

--- facade.cpp
+++ facade.cpp
@@ -1,13 +1,1 @@
-#include "stdafx.h"
-#include "math.h"
-#include "adapter.h"
-#include "iterator.cpp"
-#include "iterator.h"
-#include <iostream>
-//подключаем соответствующие классы для управления
 
-Facade::Facade()  //Facade: управляем разными интерфейсами через унифицированный
-{
-	Waters *FacadeWater = new Waters();
-	aggregate <string> *FacadeAggr = new aggregate <string> ();
-}
